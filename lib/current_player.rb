09860e78cbdd9e7befd950c317e76835249f13cb
Kyle Sanders <kyle.sanders.315@gmail.com>
2018-08-31 22:33:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 1
   board.each do |move|
-    if turn_count(move) == "X" || turn_count() == "O"
+    if turn_count(move) == "X" || turn_count(move) == "O"
     counter += 1
   end
 end
