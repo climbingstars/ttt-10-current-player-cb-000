84918645cd99c16706482affd38576abb91cc894
Kyle Sanders <kyle.sanders.315@gmail.com>
2018-08-31 22:23:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
-board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
   board.each do |move|
-    
+
 end
 
 def current_player
